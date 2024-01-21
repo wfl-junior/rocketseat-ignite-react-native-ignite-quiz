e09c34635e5c6aee27bba2e3b446f9ac5a5d7155
Wallace Júnior <wflj1997@gmail.com>
2024-01-21 19:11:49 -0300

    Instalando o React Native Reanimated

--- App.tsx
+++ App.tsx
@@ -1,26 +1,32 @@
-import { StatusBar } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
-import { Routes } from './src/routes';
-import { Loading } from './src/components/Loading';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { Fragment } from "react";
+import { StatusBar } from "react-native";
+import { Loading } from "./src/components/Loading";
+import { Routes } from "./src/routes";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
 
   if (!fontsLoaded) {
-    return (
-      <Loading />
-    );
+    return <Loading />;
   }
 
   return (
-    <>
+    <Fragment>
       <StatusBar
+        translucent
         barStyle="light-content"
         backgroundColor="transparent"
-        translucent
       />
+
       <Routes />
-    </>
+    </Fragment>
   );
 }
