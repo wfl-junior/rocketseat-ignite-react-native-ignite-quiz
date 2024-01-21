bae87d7f9906b9e33aff14309a99b27a416a06ab
Wallace Júnior <wflj1997@gmail.com>
2024-01-21 20:22:12 -0300

    Animando entrada dos itens da FlatList

--- src/components/QuizCard/index.tsx
+++ src/components/QuizCard/index.tsx
@@ -4,22 +4,29 @@ import {
   TouchableOpacityProps,
   View,
 } from "react-native";
-
+import Animated, { FadeInUp } from "react-native-reanimated";
+import { QUIZZES } from "../../data/quizzes";
 import { THEME } from "../../styles/theme";
+import { LevelBars } from "../LevelBars";
 import { styles } from "./styles";
 
-import { QUIZZES } from "../../data/quizzes";
-import { LevelBars } from "../LevelBars";
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
 
-type Props = TouchableOpacityProps & {
+interface QuizCardProps extends TouchableOpacityProps {
+  index: number;
   data: (typeof QUIZZES)[0];
-};
+}
 
-export function QuizCard({ data, ...rest }: Props) {
+export function QuizCard({ data, index, style, ...props }: QuizCardProps) {
   const Icon = data.svg;
 
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <AnimatedTouchableOpacity
+      {...props}
+      style={[styles.container, style]}
+      entering={FadeInUp.delay(index * 100)}
+    >
       <View style={styles.header}>
         <View style={styles.iconContainer}>
           {Icon && <Icon size={24} color={THEME.COLORS.GRAY_100} />}
@@ -29,6 +36,6 @@ export function QuizCard({ data, ...rest }: Props) {
       </View>
 
       <Text style={styles.title}>{data.title}</Text>
-    </TouchableOpacity>
+    </AnimatedTouchableOpacity>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,17 @@
-import { useEffect, useState } from 'react';
-import { FlatList, View } from 'react-native';
-import { Trophy } from 'phosphor-react-native';
-import { useNavigation } from '@react-navigation/native';
-
-import { Level } from '../../components/Level';
-import { Header } from '../../components/Header';
-import { QuizCard } from '../../components/QuizCard';
-
-import { styles } from './styles';
-import { QUIZZES } from '../../data/quizzes';
+import { useNavigation } from "@react-navigation/native";
+import { Trophy } from "phosphor-react-native";
+import { useEffect, useState } from "react";
+import { FlatList, View } from "react-native";
+import { Header } from "../../components/Header";
+import { Level } from "../../components/Level";
+import { QuizCard } from "../../components/QuizCard";
+import { QUIZZES } from "../../data/quizzes";
+import { styles } from "./styles";
 
 export function Home() {
-  const [quizzes, setQuizzes] = useState(QUIZZES);
-  const [levels, setLevels] = useState([1, 2, 3]);
-
   const { navigate } = useNavigation();
+  const [levels, setLevels] = useState([1, 2, 3]);
+  const [quizzes, setQuizzes] = useState(QUIZZES);
 
   function handleLevelFilter(level: number) {
     const levelAlreadySelected = levels.includes(level);
@@ -38,27 +35,45 @@ export function Home() {
         icon={Trophy}
         title="Vamos estudar"
         subtitle="Treine seus conhecimento"
-        onPress={() => navigate('history')}
+        onPress={() => navigate("history")}
       />
 
       <View style={styles.levels}>
-        <Level title="Fácil" type="EASY" onPress={() => handleLevelFilter(1)} isChecked={levels.includes(1)} />
-        <Level title="Médio" type="MEDIUM" onPress={() => handleLevelFilter(2)} isChecked={levels.includes(2)} />
-        <Level title="Difícil" type="HARD" onPress={() => handleLevelFilter(3)} isChecked={levels.includes(3)} />
+        <Level
+          title="Fácil"
+          type="EASY"
+          onPress={() => handleLevelFilter(1)}
+          isChecked={levels.includes(1)}
+        />
+
+        <Level
+          title="Médio"
+          type="MEDIUM"
+          onPress={() => handleLevelFilter(2)}
+          isChecked={levels.includes(2)}
+        />
+
+        <Level
+          title="Difícil"
+          type="HARD"
+          onPress={() => handleLevelFilter(3)}
+          isChecked={levels.includes(3)}
+        />
       </View>
 
       <FlatList
         data={quizzes}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <QuizCard
-            data={item}
-            onPress={() => navigate('quiz', { id: item.id })}
-          />
-        )}
         numColumns={2}
+        keyExtractor={item => item.id}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.cards}
+        renderItem={({ item, index }) => (
+          <QuizCard
+            data={item}
+            index={index}
+            onPress={() => navigate("quiz", { id: item.id })}
+          />
+        )}
       />
     </View>
   );
