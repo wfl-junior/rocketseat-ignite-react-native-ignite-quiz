85931b56e76c75589089b55e8e14d6b46b232fb4
Wallace Júnior <wflj1997@gmail.com>
2024-01-21 20:33:39 -0300

    Keyframes

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,36 +1,67 @@
-import { View, Text } from 'react-native';
+import { Dimensions, Text } from "react-native";
+import Animated, { Keyframe } from "react-native-reanimated";
+import { Option } from "../Option";
+import { styles } from "./styles";
 
-import { Option } from '../Option';
-import { styles } from './styles';
+const { width: screenWidth } = Dimensions.get("window");
 
-type QuestionProps = {
-  title: string;
-  alternatives: string[];
-}
+const enteringKeyframe = new Keyframe({
+  0: {
+    opacity: 0,
+    transform: [{ translateX: screenWidth }, { rotate: "90deg" }],
+  },
+  70: {
+    opacity: 0.3,
+  },
+  100: {
+    opacity: 1,
+    transform: [{ translateX: 0 }, { rotate: "0deg" }],
+  },
+});
 
-type Props = {
-  question: QuestionProps;
+const exitingKeyframe = new Keyframe({
+  from: {
+    opacity: 1,
+    transform: [{ translateX: 0 }, { rotate: "0deg" }],
+  },
+  to: {
+    opacity: 0,
+    transform: [{ translateX: -screenWidth }, { rotate: "-90deg" }],
+  },
+});
+
+interface QuestionProps {
   alternativeSelected?: number | null;
   setAlternativeSelected?: (value: number) => void;
+  question: {
+    title: string;
+    alternatives: string[];
+  };
 }
 
-export function Question({ question, alternativeSelected, setAlternativeSelected }: Props) {
+export function Question({
+  question,
+  alternativeSelected,
+  setAlternativeSelected,
+}: QuestionProps) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        {question.title}
-      </Text>
+    <Animated.View
+      style={styles.container}
+      entering={enteringKeyframe}
+      exiting={exitingKeyframe}
+    >
+      <Text style={styles.title}>{question.title}</Text>
 
-      {
-        question.alternatives.map((alternative, index) => (
-          <Option
-            key={index}
-            title={alternative}
-            checked={alternativeSelected === index}
-            onPress={() => setAlternativeSelected && setAlternativeSelected(index)}
-          />
-        ))
-      }
-    </View>
+      {question.alternatives.map((alternative, index) => (
+        <Option
+          key={index}
+          title={alternative}
+          checked={alternativeSelected === index}
+          onPress={() =>
+            setAlternativeSelected && setAlternativeSelected(index)
+          }
+        />
+      ))}
+    </Animated.View>
   );
 }
